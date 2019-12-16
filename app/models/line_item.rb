a825ef116133fe70e15c9b9df0bdc351824a95bf
rishabhguptarishi <rishabh@vinsol.com>
2019-12-16 15:03:05 +0530

    Validations

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :order, optional: true
   belongs_to :product, optional: true
   belongs_to :cart
+  validates :product_id, uniqueness: { scope: :cart_id, message: "product can come only once in a cart"}
 
   def total_price
     product.price * quantity
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,8 +3,8 @@ class Product < ApplicationRecord
   has_many :orders, through: :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :price, numericality: { greater_than_or_equal_to: 0.01 } if :price.present?
-  validates :title, presence: true, uniqueness: true
-  validates :permalink, uniqueness: true, format: {with: /\A([[:alnum:]]+\-){2,}[[:alnum:]]+\Z/i}
+  validates :title, presence: true, uniqueness: { case_sensitive: false }
+  validates :permalink, uniqueness: { case_sensitive: false }, format: {with: /\A([[:alnum:]]+\-){2,}[[:alnum:]]+\Z/i}
   validates :description, presence: true, format: {with: /\A([[:alnum:][:punct:]]+[[:space:]]){4,9}[[:alnum:][:punct:]]+\Z/}
   validates :image_url, presence: true, url: true
 #  validates :discount_price, numericality: { greater_than: :price }
