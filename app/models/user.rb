bc9fd88a7d8a5721534eecf67b5181654bde1928
rishabhguptarishi <rishabh@vinsol.com>
2019-12-17 10:59:56 +0530

    migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   after_destroy :ensure_an_admin_remains
   validates :name, presence: true, uniqueness: true
-  validates :email, uniqueness: true, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
   has_secure_password
 
   private
